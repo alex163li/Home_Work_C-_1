3e2fcc5c747d46fa27960ff9613e7e31a1128624
Alex <lialin163@yandex.ru>
2022-09-19 10:08:38 +0300

    задание 3

--- Program.cs
+++ Program.cs
@@ -17,21 +17,22 @@
 
 //Задание 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите число a");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число b");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число c");
-int c = int.Parse(Console.ReadLine());
-if (a > b && a > c)
-{
-    Console.WriteLine("Максимальное число " + a);
-}
-else if (b > a && b > c)
-{
-    Console.WriteLine("Максимальное число " + b);
-}
-else if (c > a && c > b)
-{
-    Console.WriteLine("Максимальное число " + c);
-}
+// Console.WriteLine("Введите число a");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число b");
+// int b = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число c");
+// int c = int.Parse(Console.ReadLine());
+// if (a > b && a > c)
+// {
+//     Console.WriteLine("Максимальное число " + a);
+// }
+// else if (b > a && b > c)
+// {
+//     Console.WriteLine("Максимальное число " + b);
+// }
+// else if (c > a && c > b)
+// {
+//     Console.WriteLine("Максимальное число " + c);
+// }
+
